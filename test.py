8626e500479390e3cf0fbfb25de7007c9b76fa9d
kanade-ec <iseekana@gmail.com>
2022-11-12 01:06:19 +0900

    ファイルに書き出せるように

--- test.py
+++ test.py
@@ -7,23 +7,23 @@ model = AutoModelForCausalLM.from_pretrained("rinna/japanese-gpt2-medium")
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 model = model.to(device)
 
-prompt = "ゆっくり霊夢「ゆっくり霊夢です。」ゆっくり魔理沙「ゆっくり魔理沙だぜ。」\n\n"
-num_return_sequences = 1
+prompt = "ゆっくり霊夢「ゆっくり霊夢です。」ゆっくり魔理沙「ゆっくり魔理沙だぜ。」"
+num_return_sequences = 10
 
 input_ids = tokenizer.encode(prompt, return_tensors="pt",add_special_tokens=False).to(device)
 with torch.no_grad():
     output = model.generate(
         input_ids,
-        max_length=1000,
-        min_length=100,
+        max_length=100,
         do_sample=True,
-        top_k=500,
-        top_p=0.95,
         pad_token_id=tokenizer.pad_token_id,
         bos_token_id=tokenizer.bos_token_id,
         eos_token_id=tokenizer.eos_token_id,
         num_return_sequences=num_return_sequences
     )
 decoded = tokenizer.batch_decode(output,skip_special_tokens=False)
+f = open('output.txt', 'w', encoding='UTF-8')
 for i in range(num_return_sequences):
   print(decoded[i])
+  f.write(decoded[i]+"\n")
+f.close()
